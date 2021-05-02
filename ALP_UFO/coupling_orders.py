c71dc000e333a1bf7bdfd6e475ce698180e11fa5
Yongcheng Wu <yc_wu1030@126.com>
2021-05-01 23:41:27 -0500

    Fix(Interaction Order)
    Fixing the interaction order for NP

--- ALP_UFO/coupling_orders.py
+++ ALP_UFO/coupling_orders.py
@@ -1,14 +1,14 @@
 # This file was automatically created by FeynRules 2.3.36
-# Mathematica version: 11.3.0 for Linux x86 (64-bit) (March 7, 2018)
-# Date: Sat 1 May 2021 21:52:47
+# Mathematica version: 11.3.0 for Mac OS X x86 (64-bit) (March 7, 2018)
+# Date: Sat 1 May 2021 23:31:22
 
 
 from object_library import all_orders, CouplingOrder
 
 
 NP = CouplingOrder(name = 'NP',
-                   expansion_order = 1,
-                   hierarchy = 1)
+                   expansion_order = 99,
+                   hierarchy = 2)
 
 QCD = CouplingOrder(name = 'QCD',
                     expansion_order = 99,
--- ALP_UFO/vertices.py
+++ ALP_UFO/vertices.py
@@ -1,6 +1,6 @@
 # This file was automatically created by FeynRules 2.3.36
-# Mathematica version: 11.3.0 for Linux x86 (64-bit) (March 7, 2018)
-# Date: Sat 1 May 2021 21:52:46
+# Mathematica version: 11.3.0 for Mac OS X x86 (64-bit) (March 7, 2018)
+# Date: Sat 1 May 2021 23:31:22
 
 
 from object_library import all_vertices, Vertex
